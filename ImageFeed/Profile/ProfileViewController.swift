d7b9e41d0969b6c37215a12a21b606819c08b641
tumbersa <gleb.kapustin.95@gmail.com>
2023-10-19 22:45:01 +0300

    сверстан экран профиля и добавлен соответсвующий класс

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Глеб Капустин on 19.10.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var loginNameButton: UILabel!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var avatarImageView: UIImageView!
+    
+    @IBAction private func logoutButtonTapped() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
