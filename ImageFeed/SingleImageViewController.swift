6a4515e74f35befa583bcd6bdf0f7686e60e144d
tumbersa <gleb.kapustin.95@gmail.com>
2023-10-20 12:13:11 +0300

    удален Navigation Controller  и замене на модальное окно

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -18,6 +18,9 @@ final class SingleImageViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
     
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
